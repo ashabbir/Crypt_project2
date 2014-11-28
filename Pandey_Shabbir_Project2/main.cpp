0dc60bdeb8231fbaa25f4f09ad53a5b098287f75
Ahmed Shabbir <shabbir10314@gmail.com>
2014-11-27 22:33:56 -0500

    Initial Commit

---
+++ Pandey_Shabbir_Project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pandey_Shabbir_Project2
+//
+//  Created by Ahmed Shabbir on 11/27/14.
+//  Copyright (c) 2014 NYU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
